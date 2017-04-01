2568387e0986c6b93c9788f01bb72599636c3524
Spencer Berenson <spencer@berenson.me>
2017-04-01 11:08:22 -0400

    Initial commit with base functionality.

---
+++ app.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.username}.`);
+});
+
+client.on('message', msg => {
+	if(msg.client === client) {
+		let channel = msg.channel;
+		let quote = msg.content.match(/{quote:\d+}/);
+		
+		if(quote) {
+			quote = quote[0];
+			let quoteId = quote.split(":")[1].replace("}", "");
+			channel.fetchMessages().then(msgs => {
+				let quoteSource = msgs.get(quoteId);
+				let quoteEmbed = new Discord.RichEmbed();
+				quoteEmbed.setAuthor(`${quoteSource.author.username} said: `, quoteSource.author.avatarURL).setDescription(quoteSource.content).setTimestamp(quoteSource.createdTimestamp).setURL("https://github.com/SpoonBytes/Quotecord");
+				channel.sendEmbed(quoteEmbed).then(m => channel.sendMessage(msg.content.replace(/{quote:\d+}/, "")));
+
+				msg.delete();
+			});
+		}
+	}
+});
+
+if(config.token) {
+	client.login(config.token);
+} else {
+	throw Error("Cannot login: no client token in config.json.");
+}
